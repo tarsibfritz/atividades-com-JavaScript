058405bfc107a5a5cea2f2c45b08e4a7fb269d5d
Tarsila <tarsilabarrosfritz05@outlook.com>
2024-02-20 19:07:08 -0300

    commit: atividade 7

---
+++ atividade7-somatorio/script.js
@@ -0,0 +1,8 @@
+var numero1 = parseFloat(prompt("Informe o primeiro número: "));
+var numero2 = parseFloat(prompt("Informe o segundo número: "));
+var numero3 = parseFloat(prompt("Informe o terceiro número: "));
+var numero4 = parseFloat(prompt("Informe o quarto número: "));
+var numero5 = parseFloat(prompt("Informe o quinto número: "));
+
+var somatorio = (numero1 + numero2 + numero3 + numero4 + numero5);
+console.log("Resultado da soma: " + somatorio)
