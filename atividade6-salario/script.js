d5b3eb11aa8141318340ea05b159bd24fb2512fe
Tarsila <tarsilabarrosfritz05@outlook.com>
2024-02-20 18:46:44 -0300

    commit: atividade 6

---
+++ atividade6-salario/script.js
@@ -0,0 +1,4 @@
+var nomeFuncionario = prompt("Informe o nome do funcionário: ");
+var salarioFuncionario = parseFloat(prompt("Informe o salário do funcionário: "));
+
+console.log("O funcionário " + nomeFuncionario + ", recebe um salário de R$" + salarioFuncionario + ".");
